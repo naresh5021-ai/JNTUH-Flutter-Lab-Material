0bfe3bf633d70f34870019de323cce7cf56f33c6
J Naresh Kumar <naresh5021@gmail.com>
2025-07-08 11:31:47 +0530

    add

---
+++ DART/Dart - SecondProgram.dart
@@ -0,0 +1,10 @@
+void main()
+{
+var name="Naresh Kumar";
+int age=40;
+dynamic job="Assistant Professor";
+double weight=75;
+bool doingjob=true;
+var salary=80000;
+print(job);
+}
---
+++ DART/Dart- FirstProgram.dart
@@ -0,0 +1,4 @@
+void main()
+{
+print("Hello friends, Wellcome to DART Programming Language");
+}
